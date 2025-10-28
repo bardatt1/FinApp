451f77d4924b2f9c4c9a5d9594e21ca6af737519
bardatt1 <westley.bus1@gmail.com>
2025-10-29 03:05:16 +0800

    removed node modules and out of scope directory

--- src/services/cartService.js
+++
@@ -1,6 +0,0 @@
-const cartService = {
-  getCart: () => JSON.parse(localStorage.getItem("cart")) || [],
-  saveCart: (cart) => localStorage.setItem("cart", JSON.stringify(cart)),
-};
-
-export default cartService;
